02b490a10c9e09079a347735fed086a12cb8cf79
Scott Van Duyne <scott.vanduyne@gmail.com>
2014-11-04 11:32:28 -0700

    updated database login info

--- dev/mysql.php
+++ dev/mysql.php
@@ -1,8 +1,15 @@
 <?php
+  /*
   $host  = "localhost";
   $login = "johndoe";
   $pwd   = "secret";
   $dbname= "ainews_dev";
+  mysql_connect($host, $login, $pwd) or die ("Didn't connect to mysql");
+  */
+  $host  = "localhost";
+  $login = "scottva3_simtool";
+  $pwd   = "cos-sim";
+  $dbname= "scottva3_eaf_similarity_data";
   mysql_connect($host, $login, $pwd) or die ("Didn't connect to mysql");
 ?>
 
